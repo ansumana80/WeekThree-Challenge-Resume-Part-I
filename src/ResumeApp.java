dbe9046f7c744bb21cac41a76de8905096da7ba7
ansumana80 <ansurogersbusiness@gmail.com>
2019-07-02 09:32:15 -0400

    working on resume app

--- src/ResumeApp.java
+++ src/ResumeApp.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
+
 public class ResumeApp
 {
+    public static void main (String [] args)
+    {
+        Scanner keyboard= new Scanner(System.in);
+        System.out.println("Let's fill up a resume completely");
 
+
+    }
 }
